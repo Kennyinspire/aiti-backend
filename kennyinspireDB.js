95df2c4e1d806ba4ad4bb53d773197cff3a5a94a
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2024-02-10 01:51:20 +0100

    clean up repository

--- kennyinspireDB.js
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/kennyinspire";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database Created by Kennyinspire Successfully!");
-    db.close();
-});
